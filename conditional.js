098a0f6992c3799a8a26884fa42ee4e50e11a754
gkrampah <godkram1991@gmail.com>
2023-12-28 21:32:08 +0100

    updated

---
+++ conditional.js
@@ -0,0 +1,19 @@
+let age = 30;
+if (age >= 18) {
+  console.log("You are an adult");
+} else {
+  console.log("You are a minor");
+}
+
+let day = 4;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  default:
+    console.log("Sunday");
+    break;
+}
---
+++ loop.js
@@ -0,0 +1,15 @@
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// let x = 1;
+// while (x < 10) {
+//   console.log(x);
+//   x++;
+// }
+
+let x = 1;
+do {
+  console.log(x);
+  x++;
+} while (x < 10);
